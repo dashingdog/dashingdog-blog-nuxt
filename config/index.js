78fc9e671d78628f6018b7295e1f2a3f2439296c
frankin <545520197@qq.com>
2021-07-29 00:52:43 +0800

    feat: 域名切换https

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 const Config = {
-  baseUrl: process.env.NODE_ENV === 'production' ? 'http://api.dashingdog.cn' : 'http://127.0.0.1:5000',
-  // baseUrl: 'https://api.dashingdog.cn',
-  staticPath: 'resource.shirmy.me'
+  baseUrl: process.env.NODE_ENV === 'production' ? 'https://api.dashingdog.cn' : 'http://127.0.0.1:5000',
+  staticPath: 'resource.dashingdog.cn'
 }
 
 export default Config
