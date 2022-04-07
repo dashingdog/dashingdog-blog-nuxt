dc1bf252b91b67764afd72123924516c94d5a889
frankin <38437685+dashingdog@users.noreply.github.com>
2022-04-07 10:42:21 +0800

    Update page-footer.vue
    feat: update record number

--- components/layout/page-footer/page-footer.vue
+++ components/layout/page-footer/page-footer.vue
@@ -25,7 +25,7 @@
         >© 2019-2021 <span class="logo">dashingdog</span></span
       >. All Right Reserved.
       <div class="record-number">
-        <a href="https://beian.miit.gov.cn">粤ICP备18053315号-1</a>
+        <a href="https://beian.miit.gov.cn">粤ICP备2022038948号-1</a>
       </div>
     </div>
   </footer>
